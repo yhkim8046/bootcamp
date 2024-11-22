df39a77ed969f50ff17c0bc050eb668d8714806c
Yuhwan Kim <yhkim8046@gmail.com>
2024-11-22 13:32:29 +0900

    Add CI/CD workflow

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, GitHub Actions!</h1>
+      <h1>Hello, GitHub Actions</h1>
+      <p>github actions</p>
     </div>
   );
 }
